8f2bad3017ee67f3b72e535deac847bafeb89f7f
jorgeeariass <87787484+jorgeeariass@users.noreply.github.com>
2022-07-03 16:49:00 -0500

    Update streamlit_app.py
    added pandas library
    added table with fruit macros information

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
+import pandas
+
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -8,3 +10,5 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
