c9d4e74c5edcc5153ac8b46ef028fc3f4cfa1560
jorgeeariass <87787484+jorgeeariass@users.noreply.github.com>
2022-08-03 18:29:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-import pandas
+import pandas as pd
 
 st.title('My Parents New Healthy Diner')
 
@@ -12,7 +12,7 @@ st.text('🥑 🍞 Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -22,3 +22,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 
 st.dataframe(fruits_to_show)
+
+retail_df = pd.read_csv('Retail_Sales.csv')
+st.dataframe(retail_df)
