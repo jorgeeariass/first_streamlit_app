99078fa49e259b99e52ce7eff2b1052252df129f
jorgeeariass <87787484+jorgeeariass@users.noreply.github.com>
2022-08-03 18:36:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,26 +2,13 @@ import streamlit as st
 
 import pandas as pd
 
-st.title('My Parents New Healthy Diner')
+st.title('Retail Data Analysis')
 
-# st.header('Breakfast Menu')
-# st.text('🫐 Omega 3 & Blueberry Oatmeal')
-# st.text('🥗 Kale, Spinach & Rocket Smoothie')
-# st.text('🥚 Hard-Boiled Free-Range Egg')
-# st.text('🥑 🍞 Avocado Toast')
+st.header('Breakfast Menu')
 
-# st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-# my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# my_fruit_list = my_fruit_list.set_index('Fruit')
-
-# # Let's put a pick list here so they can pick the fruit they want to include 
+# Let's put a pick list here so they can pick the fruit they want to include 
 # fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 # fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# # Display the table on the page.
-
-# st.dataframe(fruits_to_show)
 
 retail_df = pd.read_csv('Retail_Sales.csv', index_col = 'DateTimeIndex')
 retail_df.index.name = None
@@ -29,4 +16,6 @@ retail_df.index.name = None
 sales_ts = retail_df['Sales']
 sales_ts.index = pd.to_datetime(sales_ts.index, format = '%Y-%m-%d')
 
+# Display the table on the page.
+st.text('Retail Data')
 st.dataframe(retail_df)
